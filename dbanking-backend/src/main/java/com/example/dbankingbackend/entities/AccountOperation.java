ab1835153c8fdc7b196ba7faed5838284b59f3a9
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-02-13 21:28:55 +0100

    add relation between classes

--- dbanking-backend/src/main/java/com/example/dbankingbackend/entities/AccountOperation.java
+++ dbanking-backend/src/main/java/com/example/dbankingbackend/entities/AccountOperation.java
@@ -1,16 +1,20 @@
 package com.example.dbankingbackend.entities;
 
 import com.example.dbankingbackend.enumes.OperationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+@Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date oprationDate;
     private double amount;
     private OperationType operationType;
+    @ManyToOne
     private BankAccount bankAccount;
 }
--- dbanking-backend/src/main/java/com/example/dbankingbackend/entities/BankAccount.java
+++ dbanking-backend/src/main/java/com/example/dbankingbackend/entities/BankAccount.java
@@ -1,19 +1,23 @@
 package com.example.dbankingbackend.entities;
 
 import com.example.dbankingbackend.enumes.AccountStatus;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
-
+@Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class BankAccount {
+    @Id
     private String id;
     private double balance;
     private Date createdAt;
     private AccountStatus status;
+    @ManyToOne
     private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
     private List<AccountOperation> accountOperations;
 }
