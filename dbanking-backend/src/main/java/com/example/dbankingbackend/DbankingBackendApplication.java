15312dfc0c6ce2d4ab4ad26037cf0fab5b8228a1
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-02-13 20:34:20 +0100

    create backend project

---
+++ dbanking-backend/src/main/java/com/example/dbankingbackend/DbankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbankingBackendApplication.class, args);
+    }
+
+}
---
+++ dbanking-backend/src/test/java/com/example/dbankingbackend/DbankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
