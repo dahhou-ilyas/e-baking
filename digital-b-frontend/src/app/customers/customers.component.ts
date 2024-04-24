92132db36b8d1f3c019b8d4b203e4507c18591f3
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-04-24 11:11:38 +0100

    ajouté améléoration

--- digital-b-frontend/src/app/customers/customers.component.ts
+++ digital-b-frontend/src/app/customers/customers.component.ts
@@ -12,10 +12,13 @@ export class CustomersComponent implements OnInit{
   constructor(private http:HttpClient){}
 
   ngOnInit(): void {
-    this.http.get("http://localhost:8080/customers").subscribe(data=>{
-    this.customers=data;
-    },err=>{
-      console.log(err);
+    this.http.get("http://localhost:8080/customers").subscribe({
+      next:(data)=>{
+        this.customers=data;
+      },
+      error:(err)=>{
+        console.log(err);
+      }
     })
   }
 
