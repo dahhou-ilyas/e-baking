e60dfb8bb32cadc10aae18a777e8248169041edc
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-06-15 05:00:43 +0100

    add validatore

--- digital-b-frontend/src/app/new-customer/new-customer.component.ts
+++ digital-b-frontend/src/app/new-customer/new-customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Customer } from '../model/customer.model';
 import { CustomersService } from '../services/customers.service';
 
@@ -15,8 +15,8 @@ export class NewCustomerComponent implements OnInit{
 
   ngOnInit(): void {
       this.newCustomerFormGroup=this.fb.group({
-        name:this.fb.control(null),
-        mail:this.fb.control(null)
+        name:this.fb.control(null,[Validators.required]),
+        mail:this.fb.control(null,[Validators.required,Validators.email])
       });
   }
 
